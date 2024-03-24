b6fc91851bd3af8e78381f86302d27c2798e06c3
ala405 <127737904+ala405@users.noreply.github.com>
2024-03-24 03:11:31 +0100

    changes

--- src/Components/MaleHoodie.js
+++ src/Components/MaleHoodie.js
@@ -234,8 +234,8 @@ function MaleHoodie() {
 
     return (
         <> <header className="App-header">
-            <h1><Link to="/"><img src="/assets/logo_2.png" className="needle" alt="Stitch Switch" /></Link>Stitch Switch</h1>
-        </header>
+            <h1><Link to="/"><img src="/assets/logo_2.png" style={{ maxWidth: "30px", height: "auto", marginRight: "10px" }}
+                className="needle" alt="Stitch Switch" /></Link>Stitch Switch</h1>        </header>
             <div className="MaleHoodie" style={{ backgroundImage: `url(${backgroundImage})` }}>
                 <button className="button-idea" onClick={() => setSideMenuOpen(!sideMenuOpen)}>Design Ideas<img src="/assets/lamp.png" className="idea" alt='idea' /></button>
 
